554b45a2f5feece77bba59cf8d5934dbe7c2baaf
MarianaPRio <marianapires00@gmail.com>
2022-09-21 21:15:35 -0300

    ta dando ruim ainda
    Signed-off-by: MarianaPRio <marianapires00@gmail.com>

--- Trabalho Final/samefile.c
+++ Trabalho Final/samefile.c
@@ -1,68 +1,76 @@
-/*
-A tarefa é determinar se os arquivos passados como argumento para o seu programa são iguais ou diferentes.
-Para determinar se arquivos são iguais ou diferentes você deve comparar todos os bytes dos dois arquivos. 
-E, claro, se os tamanhos forem diferentes você já pode considerar que os arquivos são diferentes.
-*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+int i = 0;
 void compare(char **argv){
-
-    size_t aux; //Armazena o tamanho em bytes
-    char **arqv1, **arqv2;
-    
-    //ponteiros para os arquivos comparados
-    arqv1 = argv;
-    arqv2 = argv++;
+    i++;
+    char *buffer;
 
     //Leitura do arqv
     FILE *file1;
-    file1 = fopen(*arqv1, "r");
-    fseek(file1, 0L, SEEK_END);//acha o final do arqv1
-    long tam1 = ftell(file1);//valor atual da posicao do arqv1
-    fseek(file1, 0, SEEK_SET);//return ponteiro no inicio do arqv1
-
-    //conteudo do arqv1 p comparar
-    char *buffer1;
-    aux = (char*)malloc(sizeof(char)*tam1);
-    fseek(file1, 0, SEEK_SET); //manda o arqv1 p inicio
-    aux = fread(buffer1, 1, tam1, file1);// arqv1 p buffer apartir do inicio
-    //fread (void *buffer, int numero_de_bytes, int count   , FILE *fp);
-    //       vai p ca   , tamanho a ser lido , quantos bytes , arqv alvo
+    file1 = fopen(argv[i], "rb");
     
+    //Pega o tam
+    fseek(file1, 0, SEEK_END);//acha o final do arqv1
+    long tam1 = ftell(file1);//valor atual da posicao do arqv1
+    
+    //reinicia o ponteiro
+    fseek(file1, 0, SEEK_SET);
+    
+    //conteudo do arqv1 p comparar
+    buffer = (char*)malloc(sizeof(char) * tam1);
+    fread(buffer, tam1, 1, file1);
+    
+    //fread (void *buffer, int numero_de_bytes, int count   , FILE *fp);
+    //       vai p ca   , tamanho a ser lido ,   qnt   , arqv alvo
+    int j = i+1;
     while(0){
         //Leitura do arqv2
         FILE *file2;
-        file2 = fopen(*arqv2, "r");
+        file2 = fopen(*argv[j], "rb");
+
+        //Pega o tam
         fseek(file2, 0L, SEEK_END);//acha o final do arqv2
         long tam2 = ftell(file2);//valor atual da posicao do arqv2
-        fseek(file2, 0, SEEK_SET);//volta ao comeco do arqv
-        
+
+        //reunicia o pont
+        //fseek(file2, 0, SEEK_SET);
+
         //Comparar tam dos arqvs
         if(tam1 != tam2){
-            printf("%s %s diferentes\n", *arqv1, *arqv2);
-        }else{
+            printf("%s %s diferentes\n", argv[i], argv[j]);
+        }else{ 
             //Comparacao dos conteudos  
-            //memcmp(arqv1, arqv2, aux);
+            int same = 1;
+            char *buffer2;
+            buffer2 = (char*)malloc(sizeof(char) * tam2);
+            fread(buffer2, tam2, 1, file1);
+            for(int k = 0; k < tam1; k++){
+					if(buffer[k] != buffer2[k]){
+						same = 0;
+						break;
+					}
+				}
+            if(same == 0){
+                printf("%s %s iguais\n", argv[i], argv[j]);
+            }
+            //memcmp(argv[i], argv[j], tam1);
             //int memcmp(const void *str1, const void *str2, size_t n)
+            free(buffer2);
         }
-        
+        //novo ciclo
         fclose(file2);
-        arqv2++;
-        if(arqv2 == NULL) break;
+        if(argv[i++] == NULL) break;
     }
 
     //libera memoria
-    free(buffer1);
+    free(buffer);
     fclose(file1);
 
     //proxima comparacao
-    arqv1++;
-    if(*arqv1 == NULL){
-        return;
-    }
-    compare(arqv1);
+    if(*argv[i++] == NULL) return;
+
+    compare(argv);
 }
 
 int main(int argc,char **argv){
